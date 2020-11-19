49f143cf23ddd8442d0ed72d2239f8460a6207e5
Jamespi <123@126.com>
2020-11-19 11:10:58 +0800

    push this project

---
+++ Library/Src/Test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jamespi
+ * Date: 2020/11/19
+ * Time: 11:08
+ */
+
+namespace Jamespi\PHPAop\Src;
+
+
+class Test
+{
+    public function index(){
+        echo 1;
+    }
+}
